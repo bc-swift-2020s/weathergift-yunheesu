1f763b60fc0da6e52d75748c4aea133f8f6f02a2
Heesu Yun <Yunheesu@icloud.com>
2020-03-08 01:59:14 -0500

    Section 7.1 & 7.2 - Weather Locations List Table View

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Heesu Yun on 3/8/20.
+//  Copyright © 2020 Heesu Yun. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}

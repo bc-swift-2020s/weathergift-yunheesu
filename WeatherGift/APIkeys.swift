2031a9ea76236aad9938d1869340a713cddbf2b1
Heesu Yun <Yunheesu@icloud.com>
2020-03-19 20:33:39 +0900

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Heesu Yun on 3/19/20.
+//  Copyright © 2020 Heesu Yun. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlaceskey = "AIzaSyCHRp6SQA2X4Sf_nbApGed_S4OHYsgTRZ4"
+}

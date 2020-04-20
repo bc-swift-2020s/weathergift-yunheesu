d72b02d97669f60b3731add6e959a57cd12cb002
Heesu Yun <Yunheesu@icloud.com>
2020-04-20 13:31:20 +0900

    Section 7.21 - Freezing a TableView Cell so It Can't Be Moved or Deleted

--- WeatherGift/LocationListViewController.swift
+++ WeatherGift/LocationListViewController.swift
@@ -60,7 +60,17 @@ class LocationListViewController: UIViewController {
             addBarButton.isEnabled = false
         }
     }
+    //MARK: - tableView methods to freeze the first cell
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.row != 0 ? true : false //0가 아니면 canedit is true
+    }
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return indexPath.row != 0 ? true : false
+    }
     
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        return (proposedDestinationIndexPath.row == 0 ? sourceIndexPath : proposedDestinationIndexPath)
+    }
 }
 
 extension LocationListViewController: UITableViewDataSource, UITableViewDelegate {
